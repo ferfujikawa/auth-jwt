337bfcebb7a3cafc769516a3e2b7290872f34ff4
ferfujikawa@gmail.com <ferfujikawa@gmail.com>
2022-11-05 10:17:11 -0300

    Criação da entidade User.

---
+++ Entities/User.cs
@@ -0,0 +1,18 @@
+﻿namespace auth_jwt.Entities
+{
+    public class User
+    {
+        public Guid Id { get; private set; }
+        public string Username { get; private set; }
+        public string Password { get; private set; }
+        public string Role { get; private set; }
+
+        public User(Guid id, string username, string password, string role)
+        {
+            Id = id;
+            Username = username;
+            Password = password;
+            Role = role;
+        }
+    }
+}
